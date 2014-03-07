05301fa19e30e013f5b591032cf322275be9c0eb
Scott Hoyt <scottrhoyt@gmail.com>
2014-03-07 15:03:37 -0600

    Removed most of the debugging text.

--- Map View Controller/SHMapViewController.m
+++ Map View Controller/SHMapViewController.m
@@ -10,7 +10,7 @@
 #import <MapKit/MapKit.h>
 #import "SHPinAnnotation.h"
 
-@interface SHMapViewController () <MKMapViewDelegate, UIGestureRecognizerDelegate>
+@interface SHMapViewController () <MKMapViewDelegate>
 
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @property (strong, nonatomic) SHPinAnnotation *centerAnnotaion;
@@ -51,21 +51,9 @@
         _centerAnnotationView = [[MKPinAnnotationView alloc] initWithAnnotation:self.centerAnnotaion
                                                                 reuseIdentifier:@"centerAnnotationView"];
         _centerAnnotationView.pinColor = MKPinAnnotationColorPurple;
-        //_centerAnnotationView.backgroundColor = [UIColor blueColor];
     }
     
     return _centerAnnotationView;
-}
-
-#pragma mark - Initialization
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
 }
 
 #pragma mark - View Controller Lifecycle
@@ -74,60 +62,27 @@
 {
     [super viewDidLoad];
     self.mapView.delegate = self;
-//    [self.mapView addAnnotation:self.centerAnnotaion];
-//    
-//    UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handleGesture:)];
-//    panGesture.delegate = self;
-//    [self.mapView addGestureRecognizer:panGesture];
-//    
-//    UIPinchGestureRecognizer *pinchGesture = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(handleGesture:)];
-//    pinchGesture.delegate = self;
-//    [self.mapView addGestureRecognizer:pinchGesture];
-//	// Do any additional setup after loading the view.
-    
     [self.mapView addSubview:self.centerAnnotationView];
-    //[self.mapView addSubview:self.testView];
+    [self.mapView addSubview:self.testView];
 }
 
 #define PIN_WIDTH_OFFSET 7.75
 #define PIN_HEIGHT_OFFSET 5
 
-- (void)viewWillAppear:(BOOL)animated
+- (void)moveMapAnnotationToCoordinate:(CLLocationCoordinate2D) coordinate
 {
-    [super viewWillAppear:animated];
-    //self.centerAnnotationView.center = self.mapView.center;
-//    CGPoint centerMapPoint = [self.mapView convertCoordinate:self.mapView.centerCoordinate toPointToView:self.mapView];
-//    CGFloat xoffset = CGRectGetMidX(self.centerAnnotationView.bounds) - PIN_WIDTH_OFFSET;
-//    CGFloat yoffset = -CGRectGetMidY(self.centerAnnotationView.bounds) + PIN_HEIGHT_OFFSET;
-//    self.centerAnnotationView.center = CGPointMake(centerMapPoint.x + xoffset,
-//                                                   centerMapPoint.y + yoffset);
-//    self.centerAnnotationView.center = CGPointMake(CGRectGetMidX(self.mapView.bounds),
-//                                                   CGRectGetMidY(self.mapView.bounds));
-    
-    //self.centerAnnotationView.center = centerMapPoint;
-//    CGFloat xoffset = -CGRectGetMidX(self.centerAnnotationView.frame);
-//    CGFloat yoffset = - CGRectGetMaxY(self.centerAnnotationView.frame);
-//    xoffset = 0;
-//    yoffset = 0;
-//    
-//    self.centerAnnotationView.frame = CGRectMake(centerMapPoint.x + xoffset,
-//                                                  centerMapPoint.y + yoffset,
-//                                                  self.centerAnnotationView.frame.size.width,
-//                                                  self.centerAnnotationView.frame.size.height);
-    //self.testView.center = self.mapView.center;
-
-    //self.centerAnnotationView.center = self.mapView.center;
+    CGPoint centerMapPoint = [self.mapView convertCoordinate:coordinate toPointToView:self.mapView];
+    CGFloat xoffset = CGRectGetMidX(self.centerAnnotationView.bounds) - PIN_WIDTH_OFFSET;
+    CGFloat yoffset = -CGRectGetMidY(self.centerAnnotationView.bounds) + PIN_HEIGHT_OFFSET;
+    self.centerAnnotationView.center = CGPointMake(centerMapPoint.x + xoffset,
+                                                   centerMapPoint.y + yoffset);
 }
 
 -(void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
-    
-    CGPoint centerMapPoint = [self.mapView convertCoordinate:self.mapView.centerCoordinate toPointToView:self.mapView];
-    CGFloat xoffset = CGRectGetMidX(self.centerAnnotationView.bounds) - PIN_WIDTH_OFFSET;
-    CGFloat yoffset = -CGRectGetMidY(self.centerAnnotationView.bounds) + PIN_HEIGHT_OFFSET;
-    self.centerAnnotationView.center = CGPointMake(centerMapPoint.x + xoffset,
-                                                   centerMapPoint.y + yoffset);
+    [self moveMapAnnotationToCoordinate:self.mapView.centerCoordinate];
+    self.testView.center = self.mapView.center;
 }
 
 - (void)didReceiveMemoryWarning
@@ -136,52 +91,15 @@
     // Dispose of any resources that can be recreated.
 }
 
-#pragma mark - Gesture Recognizer Delegate Methods
-
-- (void)handleGesture:(UIGestureRecognizer *)gestureRecognizer
-{
-//    self.centerAnnotaion.coordinate = self.mapView.centerCoordinate;
-//    self.centerAnnotaion.title = [NSString stringWithFormat:@"%f, %f", self.centerAnnotaion.coordinate.latitude, self.centerAnnotaion.coordinate.longitude];
-}
-
-- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
-    //let the map view's and our gesture recognizers work at the same time...
-    return YES;
-}
 
 #pragma mark - MapView Delegate methods
 
 - (void)mapView:(MKMapView *)mapView regionDidChangeAnimated:(BOOL)animated
 {
-//    //MKCoordinateRegion region = mapView.region;
-//    self.centerAnnotaion.coordinate = mapView.centerCoordinate;
-//    self.centerAnnotaion.title = [NSString stringWithFormat:@"%f, %f", self.centerAnnotaion.coordinate.latitude, self.centerAnnotaion.coordinate.longitude];
-//    self.latLabel.text = [NSString stringWithFormat:@"%f", self.mapView.centerCoordinate.latitude];
-//    self.longLabel.text = [NSString stringWithFormat:@"%f", self.mapView.centerCoordinate.longitude];
-//    //self.centerAnnotationView.center = self.mapView.center;
-    
-    CGPoint centerMapPoint = [self.mapView convertCoordinate:self.mapView.centerCoordinate toPointToView:self.mapView];
-    CGFloat xoffset = CGRectGetMidX(self.centerAnnotationView.bounds) - PIN_WIDTH_OFFSET;
-    CGFloat yoffset = -CGRectGetMidY(self.centerAnnotationView.bounds) + PIN_HEIGHT_OFFSET;
-    self.centerAnnotationView.center = CGPointMake(centerMapPoint.x + xoffset,
-                                                   centerMapPoint.y + yoffset);
+    self.centerAnnotaion.coordinate = mapView.centerCoordinate;
+    [self moveMapAnnotationToCoordinate:mapView.centerCoordinate];
+    self.latLabel.text = [NSString stringWithFormat:@"%f", self.mapView.centerCoordinate.latitude];
+    self.longLabel.text = [NSString stringWithFormat:@"%f", self.mapView.centerCoordinate.longitude];
 }
-
-//#define PIN_REUSE_ID @"pin"
-//
-//-(MKAnnotationView *)mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation
-//{
-//    if ([annotation isKindOfClass:[SHPinAnnotation class]]) {
-//        MKPinAnnotationView *pinAnnotationView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:PIN_REUSE_ID];
-//        pinAnnotationView.pinColor = MKPinAnnotationColorGreen;
-//        
-//        return pinAnnotationView;
-//    }
-//    else
-//    {
-//        return [mapView viewForAnnotation:annotation];
-//    }
-//
-//}
 
 @end
